956a0abf85c1d0e7b087565ec6bbd6fac4930af0
bluestomr <gabriel.scherer@gmail.com>
2009-07-30 21:09:21 +0200

    updated Check module
    Ignore-this: 45e761ea6d673db2e522e83830622d9d
    
    darcs-hash:20090730190921-83633-ef49deaa74865ce868c415aa5be7c453d673b9ae

--- check.ml
+++ check.ml
@@ -2,42 +2,43 @@ let eprintf = Printf.eprintf
 
 let check_description table_name descr pgsql_descr =
   let check (correct, descr) field =
+    let field_name = Sql.Value.get field#name in
     let field_type, descr =
-      try Some (List.assoc field#name descr),
-        List.remove_assoc field#name descr
+      try Some (List.assoc field_name descr),
+        List.remove_assoc field_name descr
       with Not_found -> None, descr in
     match field_type with
       | None -> 
           eprintf "SQL Check Warning : In table %s, field %s undescribed\n"
-            table_name field#name;
+            table_name field_name;
           (correct, descr)
       | Some field_type ->
           let correct = ref correct in
-          let sql_type = Sql.sql_type_of_string field#data_type in
+          let sql_type = Sql.sql_type_of_string (Sql.Value.get field#data_type) in
           let nullable =
-            match field#nullable with
+            match Sql.Value.get field#nullable with
               | "YES" -> true
               | "NO" -> false
               | other ->
                   correct := false;
                   eprintf "SQL Check Error : In table %s, field %s has unknown 'is_nullable' value : '%s'\n"
-                    table_name field#name other;
+                    table_name field_name other;
                   true in
           (match field_type with
              | Sql.Nullable _ when not nullable ->
                  correct := false;
                  eprintf "SQL Check Error : In table %s, field %s is described as NULL but is NOT NULL\n"
-                   table_name field#name
-             | Sql.Not_null _ when nullable -> 
+                   table_name field_name
+             | Sql.Non_nullable _ when nullable -> 
                  correct := false;
                  eprintf "SQL Check Error : In table %s, field %s is described as NOT NULL but is NULL\n"
-                   table_name field#name
+                   table_name field_name
              | _ -> ());
           (match field_type with
-             | Sql.Nullable (Some t) | Sql.Not_null t when t <> sql_type->
+             | Sql.Nullable (Some t) | Sql.Non_nullable t when t <> sql_type->
                  correct := false;
                  eprintf "SQL Check Error : In table %s, field %s has incompatible types :\n\t%s in description, %s in table\n"
-                   table_name field#name (Sql.string_of_sql_type t) field#data_type
+                   table_name field_name (Sql.string_of_sql_type t) (Sql.Value.get field#data_type)
              | _ -> ());
           !correct, descr in
   let correct, left_descr = List.fold_left check (true, descr) pgsql_descr in
@@ -58,28 +59,33 @@ let check_description table_name descr pgsql_descr =
 
 let check comp =
   match comp.Sql.concrete with
-    | Sql.Query q -> failwith "unsupported query check"
+    | Sql.Selection q -> failwith "unsupported query check"
     | Sql.Table (schema, table) ->
         let table_name = Sql.string_of_table_name (schema, table) in
         let schema = match schema with
           | None -> "public"
           | Some schema -> schema in
         let pgsql_columns =
-          <:sql_table_descr< TABLE information_schema.columns (
+          <:table< information_schema.columns (
             table_schema text NOT NULL,
             table_name text NOT NULL,
             column_name text NOT NULL,
             data_type text NOT NULL,
             is_nullable text NOT NULL
           ) >> in
-        let check_comp =
-          << ( name = info.column_name, 
-               data_type = info.data_type,
-               nullable = info.is_nullable ) |
-              info <- $pgsql_columns$;
-              info.table_schema = $string:schema$;
-              info.table_name = $string:table$ >> in
-        check_description table_name comp.Sql.descr (Query.execute check_comp)
-
-          
-        
+        let check_comp = <:select<
+          { name = info.column_name;
+            data_type = info.data_type;
+            nullable = info.is_nullable } |
+          info <- $pgsql_columns$;
+          info.table_schema = $string:schema$;
+          info.table_name = $string:table$ >> in
+        let dbh = PGOCaml.connect () in
+        let check_result =
+          try `Result (check_description table_name comp.Sql.descr
+                         (Query.query dbh check_comp))
+          with exn -> `Exn exn in
+        PGOCaml.close dbh;
+        match check_result with
+          | `Result res -> res
+          | `Exn exn -> raise exn

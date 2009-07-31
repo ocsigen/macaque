5bb1e9165d5ac3d95af6ed29a1f65157c6f368d9
bluestomr <gabriel.scherer@gmail.com>
2009-07-31 06:20:52 +0200

    documentation (README) and licensing (LICENSE) information
    Ignore-this: 5ed6c6f0d583bbf7505969e3b31ad6cc
    
    darcs-hash:20090731042052-83633-11896b04f3b4fb98cb8a430d66769619b5387a10

--- base.ml
+++ base.ml
@@ -1,3 +1,9 @@
+(* This is an example of table description
+    
+    An associated sql base description with some toy data is given in
+    base.sql
+*)
+
 let recette = <:table< recette (
   id integer NOT NULL,
   nom text
--- check.ml
+++ check.ml
@@ -1,3 +1,23 @@
+(* macaque : check.ml
+    MaCaQue : Macros for Caml Queries
+    Copyright (C) 2009 Gabriel Scherer, Jérôme Vouillon
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this library; see the file LICENSE.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
+*)
+
 module ISql = Inner_sql
 
 let eprintf = Printf.eprintf

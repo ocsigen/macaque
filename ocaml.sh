edbeed61a180ec20bf930343172659a3308de76a
bluestomr <gabriel.scherer@gmail.com>
2009-08-14 18:31:50 +0200

    [build tool] ocaml.sh : small toplevel initialization script
    Ignore-this: b221514ac01e40e7c4f44b1f91ce66a7
    
    darcs-hash:20090814163150-83633-92e5695ccb2701f41ee23bce31700a3e2c9ba62d

---
+++ ocaml.sh
@@ -0,0 +1,22 @@
+OBJS="sql_base.cmo sql_internals.cmo sql_parsers.cmo sql_printers.cmo \
+      sql_flatten.cmo sql_types.cmo sql_builders.cmo sql_public.cmo \
+      inner_sql.cmo sql.cmo check.cmo query.cmo \
+      pa_comp.cmo pa_descr.cmo pa_bananas.cmo"
+
+ocamlbuild $OBJS
+
+cat > init.ml.tmp <<-EOML
+	#use "topfind";;
+	#require "pgocaml";;
+	#camlp4o;;
+EOML
+
+for f in $OBJS
+do
+    echo \#load \"_build/$f\"\;\; >> init.ml.tmp
+done
+
+echo "open Sql;;" >> init.ml.tmp
+
+rlwrap ocaml -I _build -init init.ml.tmp
+rm init.ml.tmp

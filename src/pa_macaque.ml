9d301312ceb572c73f912f17df0957f31088e589
bluestorm <gabriel.scherer@gmail.com>
2012-04-14 13:18:13 +0200

    warn and escape SQL reserved keywords
    Ignore-this: aa193c2204461de5dd64d4f30bcae16
    
    Macaque did not check that identifiers chosen by the user were valid
    SQL identifiers. Using a SQL reserved keyword resulted in a syntax
    error at runtime, when sending the SQL request to the
    database. Macaque now detects reserved keywords, warn the user, and
    escape such reserved words to avoid runtime failure.
    
    This issue was reported by Vincent Balat.
    
    It may be necessary in the future to be able to selectively disable
    escaping; if an user has an already-existing table that is marked as
    a reserved SQL keyword but for some reason accept by his/her SQL
    server, Macaque would currently still escape the table name, making
    the user unable to refer to it.
    
    darcs-hash:20120414111813-47e18-8a5a3be9e495818a8fe7cb9dc45ec87b2ae05d58

--- src/pa_macaque.ml
+++ src/pa_macaque.ml
@@ -98,6 +98,9 @@ let table_descr, seq_descr =
   let mk = CompGram.Entry.mk in
   mk "table_description", mk "seq_description"
 
+let sql_ident, sql_binder =
+  let mk = CompGram.Entry.mk in
+  mk "sql_ident", mk "sql_binder"
 
 (* The code of this whole phrase has been adapted from the
    camlp4/Camlp4Parsers/Camlp4OCamlRevisedParser.ml file of the OCaml
@@ -186,6 +189,225 @@ let prefixop,
   prefixop, infixop0, infixop1, infixop2, infixop3, infixop4, infixop5, infixop6
 
 
+
+(** Most lowercase identifier are perfectly fine as far as Macaque is
+    concerned, but some may fail at "sql query generation time"
+    because they turn out to be SQL reserved keywords. This runtime
+    failure has been reported by Vincent Valat.
+
+    We here have a list of SQL keywords and are careful to warn the
+    user when one is used, and to "escape" it into a non-reserved
+    identifier.
+
+    Macaque currently only supports PostgreSQL, so it would make sense
+    to care about PostgreSQL reserved keywords only, but this will
+    hopefully change someday in the future so I decided to also
+    consider reserved keywords as defined in the 2003 SQL standard.
+*)
+type reserved_keyword_status = {
+  reserved_in_sql2003 : bool;
+  reserved_in_postgresql : bool;
+}
+
+let reserved_keywords =
+  let keyword pgsql sql03 name =
+    if name <> String.uppercase name then
+      failwith (Printf.sprintf "Reserved keyword %S should be uppercase" name);
+    (name, { reserved_in_sql2003 = sql03; reserved_in_postgresql = pgsql }) in
+  let r pgsql sql03 name = [keyword pgsql sql03 name] in
+  let l pgsql sql03 names = List.map (keyword pgsql sql03) names in
+  (* http://www.postgresql.org/docs/8.3/static/sql-keywords-appendix.html *)
+  (* pgSQl SQL03 *) []
+  @ r false true  "ABS"
+  @ r true  true  "ALL"
+  @ r false true  "ALLOCATE"
+  @ r false true  "ALTER"
+  @ l true  false ["ANALYZE"; "ANALYSE"]
+  @ l true  true  ["AND"; "ANY"]
+  @ r false true  "ARE"
+  @ l true  true  ["ARRAY"; "AS"]
+  @ r true  false "ASC"
+  @ r false true  "ASENSITIVE"
+  @ r true  false "ASYMMETRIC"
+  @ l false true  ["AT"; "ATOMIC"]
+  @ r true  true  "AUTHORIZATION"
+  @ l false true  ["AVG"; "BEGIN"]
+  @ r true  true  "BETWEEN"
+  @ r false true  "BIGINT"
+  @ r true  true  "BINARY"
+  @ l false true  ["BLOB"; "BOOLEAN"]
+  @ r true  true  "BOTH"
+  @ l false true  ["BY"; "CALL"; "CALLED"; "CARDINALITY"; "CASCADED"]
+  @ l true  true  ["CASE"; "CAST"]
+  @ l false true  ["CEIL"; "CEILING"; "CHAR"; "CHARACTER";
+                   "CHARACTER_LENGTH"; "CHAR_LENGTH"]
+  @ r true  true  "CHECK"
+  @ l false true  ["CLOB"; "CLOSE"; "COALESCE"]
+  @ r true  true  "COLLATE"
+  @ r false true  "COLLECT"
+  @ r true  true  "COLUMN"
+  @ l false true  ["COMMIT"; "CONDITION"; "CONNECT"]
+  @ r true  true  "CONSTRAINT"
+  @ l false true  ["CONVERT"; "CORR"; "CORRESPONDING"; "COUNT";
+                   "COVAR_POP"; "COVAR_SAMP"]
+  @ l true  true  ["CREATE"; "CROSS"]
+  @ l false true  ["CUBE"; "CUME_DIST"; "CURRENT"]
+  @ r true  true  "CURRENT_DATE"
+  @ l false true  ["CURRENT_DEFAULT_TRANSFORM_GROUP"; "CURRENT_PATH"]
+  @ l true  true  ["CURRENT_ROLE"; "CURRENT_TIME"; "CURRENT_TIMESTAMP"]
+  @ r false true  "CURRENT_TRANSFORM_GROUP_FOR_TYPE"
+  @ r true  true  "CURRENT_USER"
+  @ l false true  ["CURSOR"; "CYCLE"; "DATE"; "DAY";
+                   "DEALLOCATE"; "DEC"; "DECIMAL"; "DECLARE"]
+  @ r true  true  "DEFAULT"
+  @ r true  false "DEFERRABLE"
+  @ l false true  ["DELETE"; "DENSE_RANK"; "DEREF"]
+  @ r true  false "DESC"
+  @ l false true  ["DESCRIBE"; "DETERMINISTIC"; "DISCONNECT"]
+  @ r true  true  "DISTINCT"
+  @ r true  false "DO"
+  @ l false true  ["DOUBLE"; "DROP"; "DYNAMIC"; "EACH"; "ELEMENT"]
+  @ l true  true  ["ELSE"; "END"]
+  @ l false true  ["END-EXEC"; "ESCAPE"; "EVERY"; "EXCEPT";
+                   "EXEC"; "EXECUTE"; "EXISTS"; "EXP"; "EXTERNAL"; "EXTRACT"]
+  @ r true  true  "FALSE"
+  @ l false true  ["FETCH"; "FILTER"; "FLOAT"; "FLOOR"]
+  @ l true  true  ["FOR"; "FOREIGN"];
+  @ r false true  "FREE"
+  @ r true  false "FREEZE"
+  @ l true  true  ["FROM"; "FULL"]
+  @ l false true  ["FUNCTION"; "FUSION"; "GET"; "GLOBAL"]
+  @ l true  true  ["GRANT"; "GROUP"]
+  @ r false true  "GROUPING"
+  @ r true  true  "HAVING"
+  @ l false true  ["HOLD"; "HOUR"; "IDENTITY"]
+  @ r false true  "ILIKE"
+  @ r true  true  "IN"
+  @ r false true  "INDICATOR"
+  @ r true  false "INITIALLY"
+  @ r true  true  "INNER"
+  @ l false true  ["INOUT"; "INSENSITIVE"; "INSERT";
+                   "INT"; "INTEGER"; "INTERVAL"]
+  @ l true  true  ["INTO"; "IS"]
+  @ r true  false "ISNULL"
+  @ r true  true  "JOIN"
+  @ l false true  ["LANGUAGE"; "LARGE"; "LATERAL"]
+  @ l true  true  ["LEADING"; "LEFT"; "LIKE"]
+  @ r true  false "LIMIT"
+  @ l false true  ["LN"; "LOCAL"]
+  @ l true  true  ["LOCALTIME"; "LOCALTIMESTAMP"]
+  @ l false true  ["LOWER"; "MATCH"; "MAX"; "MEMBER"; "MERGE"; "METHOD";
+                   "MIN"; "MINUTE"; "MOD"; "MODIFIES"; "MODULE"; "MONTH";
+                   "MULTISET"; "NATIONAL"]
+  @ r true  true  "NATURAL"
+  @ l false true  ["NCHAR"; "NCLOB"]
+  @ r true  true  "NEW"
+  @ l false true  ["NO"; "NONE"; "NORMALIZE"]
+  @ r true  true  "NOT"
+  @ r false true  "NOTNULL"
+  @ r true  true  "NULL"
+  @ l false true  ["NULLIF"; "NUMERIC"; "OCTET_LENGTH"; "OF"]
+  @ l true  false ["OFF"; "OFFSET"]
+  @ l true  true  ["OLD"; "ON"; "ONLY"]
+  @ r false true  "OPEN"
+  @ l true  true  ["OR"; "ORDER"]
+  @ r false true  "OUT"
+  @ l true  true  ["OUTER"; "OVERLAP"]
+  @ l false true  ["OVERLAY"; "PARAMETER"; "PARTITION";
+                   "PERCENTILE_CONT"; "PERCENTILE_DISK"; "PERCENT_RANK"]
+  @ r true  false "PLACING"
+  @ l false true  ["POSITION"; "POWER"; "PRECISION"; "PREPARE"]
+  @ r true  true  "PRIMARY"
+  @ l false true  ["PROCEDURE"; "RANGE"; "RANK"; "READS"; "REAL";
+                   "RECURSIVE"; "REF"]
+  @ r true  true  "REFERENCES"
+  @ l false true  ["REFERENCING"; "REGR_AVGX"; "REGR_AVGY"; "REGR_COUNT";
+                   "REGR_INTERCEPT"; "REGR_R2"; "REGR_SLOPE";
+                   "REGR_SXX"; "REGR_SXY"; "REGR_SYY";
+                   "RELEASE"; "RESULT"; "RETURN"]
+  @ r true  false "RETURNING"
+  @ l false true  ["RETURNS"; "REVOKE"]
+  @ r true  true  "RIGHT"
+  @ l false true  ["ROLLBACK"; "ROLLUP"; "ROW"; "ROWS"; "ROW_NUMBER";
+                   "SCOPE"; "SCROLL"; "SEARCH"; "SECOND"]
+  @ r true  true  "SELECT"
+  @ r false true  "SENSITIVE"
+  @ r true  true  "SESSION_USER"
+  @ r false true  "SET"
+  @ r true  true  "SIMILAR"
+  @ r false true  "SMALLINT"
+  @ r true  true  "SOME"
+  @ l false true  ["SPECIFIC"; "SPECIFICTYPE";
+                   "SQL"; "SQLEXCEPTION"; "SQLSTATE"; "SQLWARNING";
+                   "SQRT"; "START"; "STATIC"; "STDDEV_POP"; "STDDEV_SAMP";
+                   "SUBMULTISET"; "SUBSTRING"; "SUM"]
+  @ r true  true  "SYMMETRIC"
+  @ l false true  ["SYSTEM"; "SYSTEM_USER"]
+  @ r true  true  "TABLE"
+  @ r false true  "TABLESAMPLE"
+  @ r true  true  "THEN"
+  @ l false true  ["TIME"; "TIMESTAMP"; "TIMEZONE_HOUR"; "TIMEZONE_MINUTE"]
+  @ l true  true  ["TO"; "TRAILING"]
+  @ l false true  ["TRANSLATE"; "TRANSLATION"; "TREAT"; "TRIGGER"; "TRIM"]
+  @ r true  true  "TRUE"
+  @ r false true  "UESCAPE"
+  @ l true  true  ["UNION"; "UNIQUE"]
+  @ l false true  ["UNKNOWN"; "UNNEST"; "UPDATE"; "UPPER"]
+  @ l true  true  ["USER"; "USING"]
+  @ l false true  ["VALUE"; "VALUES"; "VARCHAR"; "VARYING";
+                   "VAR_POP"; "VAR_SAMP"]
+  @ r true  false "VERBOSE"
+  @ r true  true  "WHEN"
+  @ r false true  "WHENEVER"
+  @ r true  true  "WHERE"
+  @ l false true  ["WIDTH_BUCKET"; "WINDOW"]
+  @ r true  true  "WITH"
+  @ l false true  ["WITHIN"; "WITHOUT";
+                   "XML"; "XMLAGG"; "XMLATTRIBUTES"; "XMLBINARY";
+                   "XMLCOMMENT"; "XMLCONCAT"; "XMLELEMENT";
+                   "XMLFOREST"; "XMLNAMESPACES"; "XMLPARSE";
+                   "XMLPI"; "XMLROOT"; "XMLSERIALIZE";
+                   "YEAR"]
+
+
+(** It is rather awkward to protect SQL identifiers here, at the
+    parser level. It would make more sense to preserve the user-input
+    identifier as far as possible, that is upto the SQL query
+    generation. However, this would require sharing the keyword-base
+    code between the Camlp4 extension (which needs to have it to
+    generate the warnings) and the output code, which is rather
+    awkward to do with the .cmo loading scheme used for Camlp4
+    extensions. Doing everything in the extension is just more
+    convenient. *)
+let keyword_safe warn identifier =
+  let kw = String.uppercase identifier in
+  if not (List.mem_assoc kw reserved_keywords) then identifier
+  else
+    let status = List.assoc kw reserved_keywords in
+    let safe_identifier =
+      if not status.reserved_in_postgresql then identifier
+      else
+        (* no identifier ending with "_" is an SQL keyword *)
+        identifier ^ "_" in
+    begin match warn with
+      | `Silent -> ()
+      | `Warning loc ->
+        Syntax.print_warning loc
+          (Printf.sprintf
+             "The identifier %S is a reserved keyword %s, %s"
+             identifier
+             (match status.reserved_in_sql2003, status.reserved_in_postgresql
+              with
+                | true, true -> "in the 2003 SQL norm and in Postgresql"
+                | false, true -> "in PostgreSQL (but not in the 2003 SQL norm)"
+                | true, false -> "in the 2003 SQL norm (but not in Postgresql)"
+                | false, false -> failwith "invalid reserved keyword status")
+             (if status.reserved_in_postgresql then
+                 Printf.sprintf "so %S will be used instead." safe_identifier
+              else "it may be more future-proof to change it."))
+    end;
+    safe_identifier
+
 let () =
   Camlp4_config.antiquotations := true;
   let quote _loc str =
@@ -198,6 +420,48 @@ let () =
   let opt_list _loc = function
     | None -> (_loc, [])
     | Some thing -> thing in
+
+  (* Identifiers *)
+  EXTEND CompGram
+   GLOBAL: sql_ident sql_binder;
+
+   (* one needs to "make safe" keywords at each occurence of them,
+      but we don't wish a warning to be emitted each time for the
+      same identifier. The sql_ident/sql_binder distinction allows
+      to raise warnings for unsafe identifiers only when they are
+      in binding position.
+   *)
+  sql_ident:
+    [[ id = LIDENT -> keyword_safe `Silent id ]];
+  sql_binder:
+    [[ id = LIDENT -> keyword_safe (`Warning _loc) id ]];
+  END;
+
+ (* replacing a LIDENT by a sql_ident or sql_binder in a Camlp4
+    grammar rule is not innocuous; apparently it raises ambiguity
+    issue because paths that were left-factored with LIDENT are not
+    anymore. When a prefix common to two cases is not factored
+    anymore, the destructive behavior of Streams makes that only the
+    first case is ever tried, as it consumes the input even if it
+    ultimately fails.
+
+    Those "test_" functions are there to test, without consuming any
+    input (Stream.npeek), if the case is going to be taken, to avoid such
+    conflicts. See at use sites. *)
+  let test_in_after_ident, test_equal_after_ident =
+    let test_after_ident kw ~name:kwname =
+      CompGram.Entry.of_parser ("test_" ^ kwname ^ "_after_ident")
+        (fun strm -> match Stream.npeek 1 strm with
+          | [(LIDENT _, _)] ->
+            begin match Stream.npeek 2 strm with
+              | [_; (KEYWORD k, _)] when k = kw -> ()
+              | _ -> raise Stream.Failure
+            end
+          | _ -> raise Stream.Failure)
+    in
+    test_after_ident "in" ~name:"in",
+    test_after_ident "=" ~name:"equal"
+  in
 
   (* Comprehensions *)
   EXTEND CompGram
@@ -241,7 +505,9 @@ let () =
    offset: [[ "offset"; v = value -> v ]];
    comp_item: [[ (_, binding) = row_binding -> (_loc, Bind binding)
                | (_, cond) = value -> (_loc, Cond cond) ]];
-   row_binding: [[ handle = LIDENT; "in"; table = table ->  (_loc, (handle, table)) ]];
+   row_binding:
+     [[ test_in_after_ident; handle = sql_binder; "in"; table = table ->
+          (_loc, (handle, table)) ]];
    table: [[ `ANTIQUOT("", t) -> (_loc, quote _loc t)
            | `ANTIQUOT(id, t) ->
                (_loc, <:expr< Sql.View.$lid:id$ $quote _loc t$ >>) ]];
@@ -249,8 +515,8 @@ let () =
      [ "top" RIGHTA
          [ "match"; e = SELF; "with";
            OPT "|"; LIDENT "null"; "->"; null_case = SELF;
-           "|"; id = LIDENT; "->"; other_case = SELF ->
-             (_loc, Match (e, null_case, id, other_case))
+           "|"; id = sql_binder; "->"; other_case = SELF ->
+              (_loc, Match (e, null_case, id, other_case))
          | "if"; p = SELF; "then"; a = SELF; "else"; b = SELF ->
              (_loc, If(p, a, b)) ]
      | "||" RIGHTA [ e1 = SELF; op = infixop6; e2 = SELF -> operation _loc op [e1; e2] ]
@@ -269,7 +535,7 @@ let () =
          | (_, tup) = tuple -> (_loc, Tuple tup)
          | LIDENT "null" -> unary _loc "null"
          | LIDENT "current_timestamp" -> unary _loc "current_timestamp"
-         | id = LIDENT -> (_loc, Ident id)
+         | id = sql_ident -> (_loc, Ident id)
          | "("; (_, e) = SELF; ")" -> (_loc, e)
          | "["; e = SELF; "]" -> (_loc, Accum e) ]];
 
@@ -280,13 +546,14 @@ let () =
 
    accessor:
      [[ "."; path = field_path -> (_loc, Field path)
-      | "?"; path = [id = LIDENT -> (_loc, id)] -> (_loc, Default path) ]];
+      | "?"; path = [id = sql_ident -> (_loc, id)] -> (_loc, Default path) ]];
 
    field_path :
-     [[ path = LIST1 [id = LIDENT -> (_loc, id)] SEP "." -> (_loc, path) ]];
+     [[ path = LIST1 [id = sql_ident -> (_loc, id)] SEP "." -> (_loc, path) ]];
 
    tuple: [[ "{"; named_fields = binding_list; "}" -> (_loc, named_fields) ]];
-   binding: [[ id = LIDENT; "="; v = value -> (_loc, (id, v))
+   binding: [[ test_equal_after_ident; id = sql_binder; "="; v = value ->
+                 (_loc, (id, v))
              | v = value LEVEL "simple"; ac = accessor ->
                  let (_, default_name) = match ac with
                    | (_, Field (_, path)) -> List.hd (List.rev path)
@@ -316,7 +583,10 @@ let () =
                     "("; fields = LIST0 field_descr SEP ","; ")" ->
                       (_loc, {table_name = name;
                               field_descrs = fields}) ]];
-    field_descr: [[ name = LIDENT; typ = LIDENT;
+    (* types are not binding names but reusing existing
+       (probably keyword) identifiers, so we use LIDENT instead of
+       sql_ident *)
+    field_descr: [[ name = sql_binder; typ = LIDENT;
                     is_null = nullable_descr; def = OPT default_descr ->
                       (_loc,
                        { field_name = name;
@@ -327,10 +597,12 @@ let () =
                      | "NULL" -> true
                      | -> true ]];
     default_descr: [[ "DEFAULT"; "("; v = value; ")" -> v ]];
-    table_name: [[ schema = LIDENT; ".";  name = LIDENT -> (_loc, (Some schema, name))
-                 | name = LIDENT -> (_loc, (None, name)) ]];
+    table_name: [[ schema = sql_binder; ".";  name = sql_binder ->
+                   (_loc, (Some schema, name))
+                 | name = sql_binder -> (_loc, (None, name)) ]];
 
-    seq_descr: [[ op = [id = LIDENT -> id | -> "sequence"]; name = STRING ->
+    seq_descr: [[ op = [id = sql_ident -> id | -> "sequence"];
+                  name = STRING ->
                     (_loc, (op, name)) ]];
   END
 ;;

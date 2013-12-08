27b3762281d1607c0fac776a0ecafbbe096619af
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2013-12-08 11:59:03 +0100

    Provide a polymorphic type to array_agg

--- src/sql_types.ml
+++ src/sql_types.ml
@@ -29,14 +29,15 @@ type non_nullable
 
 class type ['t] type_info = object method typ : 't end
 class type numeric_t = object method numeric : unit end
+class type arrayable_t = object method arrayable : unit end
 
 class type unit_t = object inherit [unit] type_info end
 class type bool_t = object inherit [bool] type_info end
 class type int16_t = object inherit [int16] type_info inherit numeric_t end
-class type int32_t = object inherit [int32] type_info inherit numeric_t end
+class type int32_t = object inherit [int32] type_info inherit numeric_t inherit arrayable_t end
 class type int64_t = object inherit [int64] type_info inherit numeric_t end
 class type float_t = object inherit [float] type_info inherit numeric_t end
-class type string_t = object inherit [string] type_info end
+class type string_t = object inherit [string] type_info inherit arrayable_t end
 class type bytea_t = object inherit [bytea] type_info end
 class type time_t = object inherit [time] type_info end
 class type date_t = object inherit [date] type_info end
@@ -45,6 +46,8 @@ class type timestamptz_t = object inherit [timestamptz] type_info end
 class type interval_t = object inherit [interval] type_info end
 class type int32_array_t = object inherit [int32 array] type_info end
 class type string_array_t = object inherit [string array] type_info end
+
+class type ['t] array_t = object constraint 't = < typ : 'ty; arrayable : unit; .. > inherit ['ty array] type_info end
 
 class type ['row] row_t = object inherit ['row] type_info end
 

7f6702206588258f4cfa78cdf8ac87f776473d2a
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2013-12-21 17:07:05 +0100

    Fix the error message for IN

--- src/sql_printers.ml
+++ src/sql_printers.ml
@@ -141,7 +141,11 @@ and string_of_value (value, _) =
              | li -> " " ^ string_of_list string_of_value " " right)
     | OpTuple (_, _, [], Some default) -> sprintf "%s" default
     | OpTuple (_, op, [], None) ->
-        failwith "The operator '%s' needs a non-empty right parameter" op
+        failwith
+          (Printf.sprintf
+             "The operator '%s' needs a non-empty right parameter"
+             op
+          )
     | OpTuple (left, op, right, _) ->
         sprintf "(%s %s (%s))"
           (string_of_value left)
